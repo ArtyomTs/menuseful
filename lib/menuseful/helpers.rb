fa50667c98938e4b08ebdf482b8ec97dda753fda
Artyom Tsaplin <artyom.tsaplin@gmail.com>
2014-06-14 14:25:49 +0300

    Icons removed.

--- lib/menuseful/helpers.rb
+++ lib/menuseful/helpers.rb
@@ -12,19 +12,16 @@ module Menuseful
 
     def link_to_menu_if condition, text, url, options={}, &block
       classes = condition ? 'active' : ''
-      icon_name = options.delete(:icon)
-
-      icon = icon_name ? icon_tag(icon_name) : nil
       text = translate_views('menu', scope: text) if text.is_a?(Symbol)
-      content = content_tag(:span, text)
-
-      wrapper = block           if block_given?
-      wrapper = -> { content }  if options.delete(:wrapper) == :none
-      wrapper = -> do
-        link_to url, options.merge(title: text) do
-          concat icon
-          concat content
+      if options.delete(:wrapper) == :none
+        wrapper = -> { content }
+      else
+        wrapper = -> do
+          link_to url, options.merge(title: text) do
+            concat content_tag(:span, text)
+          end
         end
+        wrapper = block if block_given?
       end
 
       content_tag :li, (wrapper.call), class: classes
