83075dd350d74c3b9fde60d5d548e0cf2ebed38c
Artyom Tsaplin <artyom.tsaplin@gmail.com>
2014-06-14 15:39:12 +0300

    Fix.

--- lib/menuseful/railtie.rb
+++ lib/menuseful/railtie.rb
@@ -2,7 +2,7 @@ module Menuseful
   class Railtie < Rails::Railtie
     initializer "menuseful.view_helpers" do
       ActiveSupport.on_load(:action_view) do
-        require "menuseful/view_helpers/url_helper"
+        require "menuseful/view_helpers"
         include Menuseful::ViewHelpers::UrlHelper
       end
     end
